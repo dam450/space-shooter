39191b7153dbbdc591a677324364a3eb1f96278e
Dam450 <68165752+dam450@users.noreply.github.com>
2022-04-29 20:57:50 -0300

    added laser and lasershoot function
    implementa função para disparo do laser

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const yourShip = document.querySelector('.player');
 const playArea = document.querySelector('#main-play-area');
 
+
 /**
  * função de movimento e tiro da nave
- * @param {*} event evento do teclado (objeto com detalhes da tecla precionada);
+ * @param {KeyboardEvent} event evento do teclado (objeto com detalhes da tecla precionada);
  */
 function flyShip(event) {
   if (event.key === 'ArrowUp') {
@@ -15,6 +16,8 @@ function flyShip(event) {
   } else if (event.key === ' ') {
     event.preventDefault();
     fireLaser();
+  } else {
+    console.log(event);
   }
 }
 
@@ -52,4 +55,54 @@ function moveDown() {
   }
 }
 
+/**
+ * função cria o disparo de um laser no jogo
+ */
+function fireLaser() {
+  if (document.querySelectorAll('.laser').length > 2) return;
+  //canShoot = false;
+  let laser = createLaserElement();
+  playArea.appendChild(laser);
+  moveLaser(laser);
+}
+
+/**
+ * função cria um objeto laser (imagem) na tela do jogo
+ * @returns {HTMLImageElement} Objeto laser criado (imagem)
+ */
+function createLaserElement() {
+  let xPosition = parseInt(window
+    .getComputedStyle(yourShip)
+    .getPropertyValue('left'));
+
+  let yPosition = parseInt(window
+    .getComputedStyle(yourShip)
+    .getPropertyValue('top'));
+
+  let newLaser = document.createElement('img');
+
+  newLaser.src = 'img/shoot.png';
+  newLaser.classList.add('laser');
+  newLaser.style.left = `${xPosition}px`;
+  newLaser.style.top = `${yPosition - 10}px`;
+  return newLaser;
+}
+
+/**
+ * função move o laser após disparado pela area de jogo
+ * @param {HTMLImageElement} laser objeto laser (imagem)
+ */
+function moveLaser(laser) {
+  let laserInterval = setInterval(() => {
+    let xPosition = parseInt(laser.style.left);
+    if (xPosition >= 440) {
+      laser.remove();
+    } else {
+      laser.style.left = `${xPosition + 8}px`;
+    }
+  }, 10);
+}
+
 window.addEventListener('keydown', flyShip);
+
+
